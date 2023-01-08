cf0f8d757743415cf8d77eee6ec6c6b1e37e237c
Nikita Pavlov <npavlov@outlook.com>
2023-01-08 20:36:28 +0400

    init

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
+)
 
+const TOKEN = "token"
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(TOKEN)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		_, err := bot.Send(msg)
+		if err != nil {
+			log.Panic(err)
+		}
+
+	}
 }
